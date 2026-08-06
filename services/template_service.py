64d9cf2ed80692665d9ea0deb3c500e83787bfe6
saracuriel <saracurielmanzanas@gmail.com>
2026-08-06 17:50:50 +0200

    readjusting to run locally

---
+++ services/template_service.py
@@ -0,0 +1,23 @@
+"""
+Loads the Jinja2 template and renders it with the incoming JSON as context.
+"""
+
+import os
+from jinja2 import Environment, FileSystemLoader, StrictUndefined
+
+TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "..", "templates")
+
+env = Environment(
+    loader=FileSystemLoader(TEMPLATE_DIR),
+    trim_blocks=True,
+    lstrip_blocks=True,
+    extensions=["jinja2.ext.do"],
+)
+
+
+def render_turtle_template(data: dict) -> str:
+    """
+    Renders templates/record.ttl.jinja2 using `data` as the context.
+    """
+    template = env.get_template("record.ttl.jinja2")
+    return template.render(GeneralMap=data)
